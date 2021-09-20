9f5728d7bcdc3d51ece3a16395358477ef1b3115
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2021-09-20 23:32:32 +0530

    updated js for quiz
    debugged the code. set display prop of results

--- javaScript/quiz.js
+++ javaScript/quiz.js
@@ -1,6 +1,6 @@
 const quizForm = document.querySelector('#form-quiz');
 const submitButton = document.querySelector('#button-submit');
-const retultMessage = document.querySelectorAll(".result");
+const resultMessage = document.querySelectorAll(".result");
 const message = document.querySelector('.message');
 const messageDiv = document.querySelector(".div-message");
 
@@ -19,18 +19,22 @@ function calculateScore () {
 
     for (let value of formAnsweres.values()) {
         if (value === quizAnswers[index]) {
-            retultMessage[index].innerText = "Correct answer!!"
-            retultMessage[index].style.backgroundColor = "#78d8db"
+         resultMessage[index].innerText = "Correct answer!!"
+         resultMessage[index].style.backgroundColor = "#78d8db"
             score++;
         } else {
-            retultMessage[index].innerText = "Wrong answer.\nCorrect answer is: " +quizAnswers[index];
-            retultMessage[index].style.backgroundColor = "#d9af39"
+         resultMessage[index].innerText = "Wrong answer.\nCorrect answer is: " +quizAnswers[index];
+         resultMessage[index].style.backgroundColor = "#d9af39"
 
         }
         index++;    
     }
 
     if (index === 10) {
+        for (var i = 0; i < index; i++) {
+            resultMessage[i].style.display = "block";
+        }
+
         message.innerText = "Your score is: " +score+ "/10";
     } else {
         message.innerText = "Select an option for each question to continue";
